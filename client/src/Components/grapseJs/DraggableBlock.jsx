c549ecb1474f519e6350be934736b95b0394f422
rahul06062001 <rahul.rj0606@gmail.com>
2024-07-24 17:52:06 +0530

    asdfasf

--- client/src/Components/grapseJs/DraggableBlock.jsx
+++ client/src/Components/grapseJs/DraggableBlock.jsx
@@ -25,7 +25,7 @@ const customBlocks = [
   {
       id: 'Code',
       label: '<svg data-wf-icon="AddPanelCodeBlock64Icon" width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 15C12 13.3431 13.3431 12 15 12H49C50.6569 12 52 13.3431 52 15V49C52 50.6569 50.6569 52 49 52H15C13.3431 52 12 50.6569 12 49V15Z" fill="white" fill-opacity="0.1"></path><path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd" d="M19 52L19 12L20 12L20 52H19Z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M15 13H49C50.1046 13 51 13.8954 51 15V49C51 50.1046 50.1046 51 49 51H15C13.8954 51 13 50.1046 13 49V15C13 13.8954 13.8954 13 15 13ZM12 15C12 13.3431 13.3431 12 15 12H49C50.6569 12 52 13.3431 52 15V49C52 50.6569 50.6569 52 49 52H15C13.3431 52 12 50.6569 12 49V15ZM33 18H24V17H33V18ZM41 23H29V22H41V23ZM43 23H47V22H43V23ZM35 28V27H47V28H35ZM29 28H33V27H29V28ZM33 33H24V32H33V33Z" fill="currentColor"></path></svg>Code',
-      content: `<div class="codebg"><h3>Example</h3>
+      content: `<div class="codebg"><h3>Example</h3>&nbsp;&nbsp;
         <style type="text/css">
           .codebg {
             background:#f6f6f6;
@@ -294,3 +294,11 @@ const customBlocks = [
 
 
 export default customBlocks;
+
+
+
+
+//  inculde(ceode)
+
+//   exampe class jtc
+// exampletwo class jtc
